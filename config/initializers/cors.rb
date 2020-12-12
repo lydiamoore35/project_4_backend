5700c723d19115fc584cf17e91b504776a373a6d
Lydia Moore <lydiamoore35@gmail.com>
2020-12-12 11:37:10 -0700

    deploying

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,8 +5,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
